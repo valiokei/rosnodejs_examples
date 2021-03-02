7d6572991b8adaf185ec7515d28c0b33e64ee129
Valerio Brunacci <valiokei@gmail.com>
2021-03-02 18:25:59 +0100

    Add multiple video stream handling in talker and listener

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,14 +1,42 @@
 #!/usr/bin/env node
 
+
+
+/************************************************************************
+ Copyright (c) 2017, Rethink Robotics
+ Copyright (c) 2017, Ian McMahon
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+************************************************************************/
+
 'use strict';
 const { Console } = require('console');
+/**
+ * This example demonstrates simple receiving of messages over the ROS system.
+ */
 
-// opening the udp stream to sending data for gstreamer pipeline consumption
+
+
+
+
 const dgram = require('dgram');
-const client = dgram.createSocket('udp4');
+const client1 = dgram.createSocket('udp4');
+const client2 = dgram.createSocket('udp4');
+
 /////////////// SETTINGS    ///////////////
 
-const serverPort = 5000;
+const serverPort1 = 5000;
+const serverPort2 = 5001;
 const serverAddress = "127.0.0.1";
 
 /////////////// SETTINGS    ///////////////
@@ -17,26 +45,44 @@ const serverAddress = "127.0.0.1";
 const rosnodejs = require('rosnodejs');
 // Requires the std_msgs message package
 const std_msgs = rosnodejs.require('std_msgs').msg;
+const fs = require('fs');
 
-
+let a = 0
+let b = 0
 function listener() {
   // Register node with ROS master
   rosnodejs.initNode('/listener_node')
     .then((rosNode) => {
       // Create ROS subscriber on the 'chatter' topic expecting String messages
-      let sub = rosNode.subscribe('/chatter', std_msgs.UInt8MultiArray,
-        (data) => { 
+      let sub1 = rosNode.subscribe('/stream1', std_msgs.UInt8MultiArray,
+        (data) => { // define callback execution
           // rosnodejs.log.info('I heard: [' + data.data + ']');
-          // console.log(data.data)
-          client.send(data.data, serverPort, serverAddress);
+
+          
+          client1.send(data.data, serverPort1, serverAddress);
+          a+=1;
+          console.log("data received " + a)
         }
       );
+      let sub2 = rosNode.subscribe('/stream2', std_msgs.UInt8MultiArray,
+      (data) => { // define callback execution
+        // rosnodejs.log.info('I heard: [' + data.data + ']');
+        
+        client2.send(data.data, serverPort2, serverAddress);
+        b+=1;
+        console.log("data received " + b)
+      }
+    );
     });
 }
 
 if (require.main === module) {
 
-  client.on('close', function () {
+  client1.on('close', function () {
+    console.log('Client UDP socket closed : BYE!')
+  });
+
+  client2.on('close', function () {
     console.log('Client UDP socket closed : BYE!')
   });
 
--- scripts/talker.js
+++ scripts/talker.js
@@ -1,19 +1,40 @@
 #!/usr/bin/env node
 
+/************************************************************************
+ Copyright (c) 2017, Rethink Robotics
+ Copyright (c) 2017, Ian McMahon
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+************************************************************************/
 
 'use strict';
+/**
+ * This example demonstrates simple sending of messages over the ROS system.
+ */
 
-// open connection to the udp socket for getting incoming data
 const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+const server1 = dgram.createSocket('udp4');
+const server2 = dgram.createSocket('udp4');
 
 
 /////////////// SETTINGS    ///////////////
 
-const serverPort = 5000;
+const serverPort1 = 5000;
+const serverPort2 = 5001;
 const serverAddress = "127.0.0.1";
 
 /////////////// SETTINGS    ///////////////
+
 
 
 // Require rosnodejs itself
@@ -26,15 +47,24 @@ function talker() {
   rosnodejs.initNode('/talker_node')
     .then((rosNode) => {
       // Create ROS publisher on the 'chatter' topic with String message
-      let pub = rosNode.advertise('/chatter', std_msgs.UInt8MultiArray);
-      const msg = new std_msgs.UInt8MultiArray();
+      let pub1 = rosNode.advertise('/stream1', std_msgs.UInt8MultiArray);
+      let pub2 = rosNode.advertise('/stream2', std_msgs.UInt8MultiArray);
+      const msg1 = new std_msgs.UInt8MultiArray();
+      const msg2 = new std_msgs.UInt8MultiArray();
 
-      console.log("rosnodejs has initialized the /talker_node")
+      console.log("rosnodejs has inistialized the /talker_node")
 
-      server.on('message', (socketmsg, rinfo) => {
+      server1.on('message', (socketmsg, rinfo) => {
         // console.log(`server got: ${socketmsg} from ${rinfo.address}:${rinfo.port}`);      
-        msg.data = socketmsg;
-        pub.publish(msg);
+        msg1.data = socketmsg;
+        pub1.publish(msg1);
+        //rosnodejs.log.info('I said: [' + msg.data + ']');
+      });
+
+      server2.on('message', (socketmsg, rinfo) => {
+        // console.log(`server got: ${socketmsg} from ${rinfo.address}:${rinfo.port}`);      
+        msg2.data = socketmsg;
+        pub2.publish(msg2);
         //rosnodejs.log.info('I said: [' + msg.data + ']');
       });
     });
@@ -42,23 +72,39 @@ function talker() {
 
 if (require.main === module) {
 
-  server.on('error', (err) => {
+  server1.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
-    server.close();
+    server1.close();
   });
 
 
 
-  server.on('listening', () => {
-    const address = server.address();
+  server1.on('listening', () => {
+    const address = server1.address();
     console.log(`server listening ${address.address}:${address.port}`);
   });
 
-  server.bind({
+  server1.bind({
     address: serverAddress,
-    port: serverPort
+    port: serverPort1
   });
 
+  server2.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server1.close();
+  });
+
+
+
+  server2.on('listening', () => {
+    const address = server2.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+  });
+
+  server2.bind({
+    address: serverAddress,
+    port: serverPort2
+  });
 
   // Invoke Main Talker Function
   talker();
